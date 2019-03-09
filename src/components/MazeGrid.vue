20c34d4edfe95e77b1c97a1cacf660d8aac89e89
Felipe Flor <f.flor@vehikl.com>
2019-03-09 17:40:56 -0500

    Allow MazeManager to change contents of the Grid

--- src/components/MazeGrid.vue
+++ src/components/MazeGrid.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="maze-grid"
          :style="`grid-template-columns: repeat(${this.columnCount}, auto)`">
-        <maze-cell v-for="(content, index) in cellsContent"
-                   :key="index"
+        <maze-cell v-for="(content, position) in cellsContent"
+                   :key="position"
                    :content="content"
-                   @click="changeContent(index)"
+                   @click="onCellClicked({content, position})"
                    class="maze-cell">
         </maze-cell>
     </div>
@@ -14,14 +14,46 @@
     import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
     import MazeCell, {CellContent} from './MazeCell.vue';
 
+    export {CellContent};
+
+    export interface CellData {
+        content: CellContent;
+        position: number;
+    }
+
+    export interface MazeGridAction {
+        id: number;
+        cellData: CellData;
+    }
+
+    export const EMPTY_ACTION: MazeGridAction = {
+        id: -1,
+        cellData: {
+            content: CellContent.Nothing,
+            position: -1,
+        },
+    };
+
     @Component({components: {MazeCell}})
     export default class MazeGrid extends Vue {
         @Prop({default: 6}) private gridSize!: number;
-        private CellContent = CellContent;
+        @Prop({default: () => EMPTY_ACTION}) private actionRequest!: MazeGridAction;
+
+        // Properties
         private cellsContent: CellContent[] = [];
 
-        private created(): void {
-            this.populateGridWithEmptyCells();
+        private get columnCount(): number {
+            return this.gridSize;
+        }
+
+        // Watchers
+        @Watch('actionRequest.id')
+        public changeCellContent(): void {
+            Vue.set(
+                this.cellsContent,
+                this.actionRequest.cellData.position,
+                this.actionRequest.cellData.content,
+            );
         }
 
         @Watch('gridSize')
@@ -29,12 +61,13 @@
             this.cellsContent = 'x'.repeat(this.gridSize ** 2).split('').map(() => CellContent.Nothing);
         }
 
-        private changeContent(cellIndex: number): void {
-            Vue.set(this.cellsContent, cellIndex, CellContent.Wall);
+        // Event Handlers
+        private created(): void {
+            this.populateGridWithEmptyCells();
         }
 
-        private get columnCount(): number {
-            return this.gridSize;
+        private onCellClicked(currentState: CellData): void {
+            this.$emit('cell-clicked', currentState);
         }
     }
 </script>
--- src/components/MazeManager.vue
+++ src/components/MazeManager.vue
@@ -1,32 +1,56 @@
+import {CellContent} from './MazeCell';
 <template>
     <div class="maze-manager">
         <div class="configurations">
-            <button>Load Preset</button>
-
+            <div>
+                <div>
+                    <button>Load</button>
+                    <button>Export</button>
+                </div>
+                <button>Get Example</button>
+            </div>
             <div>
                 <span class="grid-size-label">Grid Size</span>
                 <button id="decrease-maze-size"
                         :disabled="gridSize <= 7"
-                        @click="gridSize--">-</button>
+                        @click="gridSize--">-
+                </button>
                 <button id="increase-maze-size"
-                        @click="gridSize++">+</button>
+                        @click="gridSize++">+
+                </button>
             </div>
 
-            <button>Play</button>
+            <div>
+                <button>Play</button>
+            </div>
         </div>
 
-        <maze-grid :grid-size="gridSize"/>
+        <maze-grid :grid-size="gridSize"
+                   :action-request="newAction"
+                   @cell-clicked="onCellClicked"/>
     </div>
 </template>
 
 <script lang="ts">
-    import {Component, Prop, Vue} from 'vue-property-decorator';
-    import {CellContent} from './MazeCell.vue';
-    import MazeGrid from './MazeGrid.vue';
+    import {Component, Vue} from 'vue-property-decorator';
+    import MazeGrid, {CellContent, MazeGridAction, CellData, EMPTY_ACTION} from './MazeGrid.vue';
 
     @Component({components: {MazeGrid}})
     export default class MazeManager extends Vue {
         private gridSize: number = 10;
+        private newAction: MazeGridAction = EMPTY_ACTION;
+        private actionsCount: number = 0;
+
+        private onCellClicked(currentState: CellData) {
+            if (currentState.content === CellContent.Nothing) {
+                this.changeCellContent(currentState.position, CellContent.Wall);
+            }
+        }
+
+        private changeCellContent(position: number, content: CellContent) {
+            this.newAction.cellData = {position, content};
+            Vue.set(this.newAction, 'id', ++this.actionsCount);
+        }
     }
 </script>
 
