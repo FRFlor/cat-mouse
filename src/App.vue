db7cd499b8ccec1b5fcf430a05e8906941410dc8
Felipe Flor <f.flor@vehikl.com>
2019-03-09 14:06:26 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <maze-grid></maze-grid>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import MazeGrid from './components/MazeGrid.vue';
+
+@Component({
+  components: {
+      MazeGrid,
+  },
+})
+export default class App extends Vue {}
+</script>
---
+++ src/components/MazeGrid.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="maze-grid">
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class MazeGrid extends Vue {
+    }
+</script>
+
+<style scoped lang="scss">
+    .maze-grid {
+        background: red;
+        height: 150px;
+        width: 150px;
+    }
+</style>
