416d2e71a5bfefd2387abdd61b4331b9a27bb190
Felipe Flor <f.flor@vehikl.com>
2019-03-09 14:42:31 -0500

    Allow MazeCells to have content

---
+++ src/components/MazeCell.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="maze-cell" v-text="content">
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    export enum CellContent {
+        Nothing = '',
+        Wall = 'W',
+        Cat = 'C',
+        Mouse = 'M',
+        Cheese = 'ch',
+    }
+
+    @Component
+    export default class MazeCell extends Vue {
+        @Prop({default: () => CellContent.Nothing}) public content!: CellContent;
+    }
+</script>
+
+<style scoped lang="scss">
+    .maze-cell {
+        background: blue;
+        border: orange 2px solid;
+        height: 40px;
+        width: 40px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
--- src/components/MazeGrid.vue
+++ src/components/MazeGrid.vue
@@ -1,23 +1,35 @@
 <template>
     <div class="maze-grid"
          :style="`grid-template-columns: repeat(${this.columnCount}, auto)`">
-        <div v-for="(cell, index) in cells"
-             :key="index"
-             class="maze-cell">
+        <maze-cell v-for="(content, index) in cellsContent"
+                   :key="index"
+                   :content="content"
+                   class="maze-cell">
 
-        </div>
+        </maze-cell>
     </div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import MazeCell, {CellContent} from './MazeCell.vue';
 
-    @Component
+    @Component({components: {MazeCell}})
     export default class MazeGrid extends Vue {
-        private cells: any[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        @Prop({default: 5}) public gridSize!: number;
+
+        private cellsContent: CellContent[] = [];
+
+        private created(): void {
+            this.populateGridWithEmptyCells();
+        }
+
+        private populateGridWithEmptyCells(): void {
+            this.cellsContent = 'x'.repeat(this.gridSize ** 2).split('').map(() => CellContent.Nothing);
+        }
 
         private get columnCount(): number {
-            return Math.floor(this.cells.length ** 0.5);
+            return this.gridSize;
         }
     }
 </script>
@@ -25,11 +37,5 @@
 <style scoped lang="scss">
     .maze-grid {
         display: grid;
-        .maze-cell {
-            background: blue;
-            border: orange 2px solid;
-            height: 20px;
-            width: 20px;
-        }
     }
 </style>
