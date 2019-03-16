4e3f48731788f5adfbc5dc32e198091e4325adbf
Felipe Flor <f.flor@vehikl.com>
2019-03-16 16:43:34 -0400

    Cause the cat to capture everything it can, even if one or more targets are unreachable

--- src/classes/Cat.ts
+++ src/classes/Cat.ts
@@ -15,6 +15,7 @@ export default class Cat {
     private grid: GridCell[];
     private graph: Graph;
     private desiredPath: GridCell[] = [];
+    private unreacheableTargets: GridCell[] = [];
 
     constructor(startCatCell: GridCell, grid: GridCell[]) {
         this.grid = grid;
@@ -39,25 +40,45 @@ export default class Cat {
     }
 
     private get thereAreNoTargetsLeft(): boolean {
-        return this.milkCells.length === 0 && this.mouseCell === undefined;
+        return this.reachableMilkCells.length === 0 && this.reachableMouseCell === undefined;
     }
 
-    private get milkCells(): GridCell[] {
+    private get allMilkCells(): GridCell[] {
         return this.grid.filter((cell: GridCell) => cell.content === CellContent.Milk);
+    }
+
+    private get reachableMilkCells(): GridCell[] {
+        return this.allMilkCells.filter((cell: GridCell) => this.unreacheableTargets.indexOf(cell) === -1);
     }
 
     private get mouseCell(): GridCell | undefined {
         return this.grid.find((cell: GridCell) => cell.content === CellContent.Mouse);
     }
 
+    private get reachableMouseCell(): GridCell | undefined {
+        // No mouse cell?
+        if (this.mouseCell === undefined) {
+            return undefined;
+        }
+
+        return this.unreacheableTargets.indexOf(this.mouseCell) === -1
+            ? this.mouseCell
+            : undefined; // Mouse cell is unreachable
+    }
+
     public start() {
-        this.graph.compose();
+        this.graph.compose(); // Build Graph
+        this.unreacheableTargets = this.getUnreachableTargets();
         this.desiredPath = this.getShortestPath();
     }
 
-    public setCourseTo(destination: GridCell) {
-        this.desiredPath = this.getPath(this.cell, destination);
+    private getUnreachableTargets(): GridCell[] {
+        const allTargets = this.allMilkCells;
+        if (this.mouseCell !== undefined) {
+            allTargets.push(this.mouseCell);
+        }
 
+        return allTargets.filter((target: GridCell) => this.getPath(this.cell, target).length === 0);
     }
 
     private getPath(from: GridCell, to: GridCell): GridCell[] {
@@ -69,15 +90,9 @@ export default class Cat {
     }
 
     private getShortestPath(): GridCell[] {
-        // With no mouse, there's no path
-        if (this.mouseCell === undefined) {
+        // With no reachable mouse or milk, just give up
+        if (this.reachableMouseCell === undefined && this.reachableMilkCells.length === 0) {
             return [];
-        }
-
-        // With just a mouse, there's only one obvious shortest path
-        const milkCell: GridCell | undefined = this.milkCells.shift();
-        if (milkCell === undefined) {
-            return this.getPath(this.cell, this.mouseCell);
         }
 
         const allStrategies: MoveStrategy[] = [];
@@ -85,9 +100,13 @@ export default class Cat {
         // Consider all possible ordering of capturing the milk
         // Each of those orders is a new strategy for the cat
         const permutation = require('array-permutation');
-        const iterator = permutation(this.milkCells);
+        const iterator = permutation(this.reachableMilkCells);
         for (const targets of iterator) {
-            targets.push(this.mouseCell); // The mouse is always the last target
+            // If the mouse is reachable, make it the last target
+            if (this.reachableMouseCell !== undefined) {
+                targets.push(this.reachableMouseCell);
+            }
+
             allStrategies.push({
                 targets,
                 path: [],
@@ -95,19 +114,23 @@ export default class Cat {
         }
 
         // If there were no permutations generated
-        // it means that there's only a single milk and a mouse.
+        // it means that there's only a single path.
         if (allStrategies.length === 0) {
+            const targets = this.reachableMilkCells;
+            if (this.reachableMouseCell !== undefined) {
+                targets.push(this.reachableMouseCell);
+            }
+
             allStrategies.push({
-                targets: [milkCell, this.mouseCell],
+                targets,
                 path: [],
             });
         }
 
-        // If one of the milks or the mouse are unreachable, give up
-        let isImpossible: boolean = false;
 
-        // @ts-ignore
-        const mousePosition: number = this.mouseCell.position;
+        const mousePosition: number = (this.reachableMouseCell === undefined)
+            ? -1
+            : this.reachableMouseCell.position;
 
         // Test each strategy and see how many moves each of them need
         allStrategies.forEach((strategy: MoveStrategy) => {
@@ -118,18 +141,9 @@ export default class Cat {
                     return;
                 }
 
-                if (isImpossible) {
-                    return;
-                }
-
                 const pathForTarget: GridCell[] = this.getPath(currentPosition, target);
-                if (pathForTarget.length === 0) {
-                    strategy.path = [];
-                    isImpossible = true;
-                    return;
-                }
 
-                // Poor mouse got eaten by accident
+                // Check to see if the poor mouse got eaten by accident
                 if (pathForTarget.find((cell: GridCell) => cell.position === mousePosition) !== undefined) {
                     mouseWasEatenByAccident = true;
                 }
@@ -138,10 +152,6 @@ export default class Cat {
                 currentPosition = strategy.path[strategy.path.length - 1];
             });
         });
-
-        if (isImpossible) {
-            return [];
-        }
 
         // Sort strategies by number of moves needed for victory
         allStrategies.sort((a: MoveStrategy, b: MoveStrategy) => {
